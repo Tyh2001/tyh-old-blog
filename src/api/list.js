53e6155e2da89489637cbb77000ffe7031b8a3a3
tyh666999 <1469442737@qq.com>
2021-03-08 23:15:20 +0800

    文档内容展示

--- src/api/list.js
+++ src/api/list.js
@@ -4,10 +4,18 @@
 
 import request from '@/utils/request'
 
-// 登录
+// 获取我发布的博客内容
 export const getBlogList = () => {
   return request({
     method: 'GET',
     url: '/tyh/bloglist1'
+  })
+}
+
+// 获取vue技术文章
+export const getVueDoc = () => {
+  return request({
+    method: 'GET',
+    url: '/tyh/document/vue'
   })
 }
--- src/views/ceshi/index.vue
+++ src/views/ceshi/index.vue
@@ -1,26 +1,52 @@
 <template>
   <div id="ceshi">
-    <p>微博图片地址</p>
-    <img src="https://wx1.sinaimg.cn/large/007LmPOmly1go95lsauwvj30sc0p1qpf.jpg" alt="">
-    <p>知乎地址</p>
-    <img src="https://pic2.zhimg.com/50/v2-2b6eda8ba79dd37e344909a53a85f005_hd.jpg?source=1940ef5c" alt="">
-    <p>qq空间地址</p>
-    <img src="http://r.photo.store.qq.com/psc?/V52ZsGYR2rB2dN0Ljym24DcBjp0pKw0B/45NBuzDIW489QBoVep5mceTySHJwdTLgBOpqYACW0fwJLlXQ4KavpQwEU6kfQhOJjxyWJFzcqktOY*CcTocjzJJr8wOxEo8Wd7KiqZoKxAI!/r" alt="">
+    <mavon-editor v-model="value"/>
   </div>
 </template>
 <script>
+import { getVueDoc } from '@/api/list'
+import Vue from 'vue'
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+Vue.use(mavonEditor)
 export default {
+  name: 'ceshiIndex',
   data () {
-    return {}
+    return {
+      value: ''
+    }
+  },
+  created () {
+    this.loadgetVueDoc()
+  },
+  methods: {
+    async loadgetVueDoc () {
+      const { data } = await getVueDoc()
+      this.value = data.data.vue.document
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
 #ceshi {
-  background: #fff;
+  // background: #fff;
 }
 img {
   height: 200px;
+}
+// 下下面为富文本编辑器需要移除的样式
+/deep/ .v-note-edit,
+/deep/ .v-note-op {
+  display: none !important;
+}
+// 整个大盒子的宽度
+.v-note-wrapper {
+  width: 700px;
+  margin: auto;
+  z-index: 500;
+}
+/deep/ .v-note-show {
+  flex: 1 !important;
 }
 </style>
