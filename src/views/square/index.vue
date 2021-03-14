e232d6ba5be196565325c34c9e631ce59e7005d9
tyh666999 <1469442737@qq.com>
2021-03-15 00:37:31 +0800

    音频控件测试

--- src/views/square/index.vue
+++ src/views/square/index.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="squareIndex">
-    <h1>广场页面。。。后续更新。。。</h1>
+    <button @click="Time()">开始</button>
+    <el-progress
+      :text-inside="true"
+      :stroke-width="26"
+      :percentage="speed"
+    />
+    <audio controls>
+      <source src="./music/下山 - 要不要买菜.mp3" type="audio/mpeg">
+    </audio>
+    <audio controls>
+      <source src="./music/芊芊 (女生版) - 玄觞.mp3" type="audio/mpeg">
+    </audio>
+    <audio controls>
+      <source src="./music/说客 - 音阙诗听 _ 新乐尘符.flac" type="audio/mpeg">
+    </audio>
   </div>
 </template>
 
@@ -10,18 +24,30 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      speed: 0
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    // x * 100 = 235000
+    Time: function () {
+      setInterval(() => {
+        if (this.speed < 100) {
+          this.speed++
+        }
+      }, 2350)
+    }
+  }
 }
 </script>
 
 <style scoped lang="less">
 #squareIndex {
+  height: 500px;
   background: #fff;
 }
 </style>
