a1630c329f0f5f8376967ee1e74dec8cf967c06a
tyh666999 <1469442737@qq.com>
2021-04-24 22:49:32 +0800

    动态也加入视频背景

--- src/views/blog/index.vue
+++ src/views/blog/index.vue
@@ -6,6 +6,10 @@
     <el-backtop>
       <i class="tyh tyh-huojian" />
     </el-backtop>
+
+    <div class="video">
+      <video src="./video/背景视频.mp4" autoplay loop muted></video>
+    </div>
   </div>
 </template>
 
@@ -23,6 +27,23 @@ export default {
 
 <style lang="less" scoped>
 #blogIndex {
+  // width:100vw;
+  // height: 100vh;
+  width: 100%;
+  height: 100%;
   padding: 15px 0 50px;
+  .video {
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    video {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
 }
 </style>
